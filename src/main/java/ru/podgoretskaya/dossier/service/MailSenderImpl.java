c59a84519c957de111cff8a85e61d05e1b39928c
Olya-Breskina <breskina-olya@mail.ru>
2023-02-08 18:20:08 +0300

    жобавила  сообщения писем

--- src/main/java/ru/podgoretskaya/dossier/service/MailSenderImpl.java
+++ src/main/java/ru/podgoretskaya/dossier/service/MailSenderImpl.java
@@ -27,6 +27,16 @@ public class MailSenderImpl implements MailSender {
     }
 
     private String buildText(EmailMessage emailMessage) {
-        return String.format("Добрый день,заявки с № %s: %s", emailMessage.getApplicationId(), emailMessage.getTheme());
+        String  messageFoSend = new String();
+        switch (emailMessage.getTheme()) {
+            case FINISH_REGISTRATION:  messageFoSend= String.format("Добрый день,заявка на кредит № %s, пожалуйста закончите регистрацию", emailMessage.getApplicationId());break;
+            case SEND_SES: messageFoSend= String.format("Добрый день,заявка на кредит № %s, введите код подтверждения", emailMessage.getApplicationId());break;
+            case CREDIT_ISSUED:messageFoSend= String.format("Добрый день,вам выдан кредит");break;
+            case SEND_DOCUMENTS:messageFoSend= String.format("Добрый день,заявка на кредит № %s, отправить документы", emailMessage.getApplicationId());break;
+            case CREATE_DOCUMENTS:messageFoSend= String.format("Добрый день,заявка на кредит № %s, создать документы", emailMessage.getApplicationId());break;
+            case APPLICATION_DENIED:messageFoSend= String.format("Добрый день,заявка на кредит № %s отклонена", emailMessage.getApplicationId());break;
+            default: messageFoSend= String.format("Добрый день,заявка  отклонена");
+        }
+        return messageFoSend;
     }
 }
